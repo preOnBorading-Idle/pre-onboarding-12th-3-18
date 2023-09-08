bb64795b2f29ae5986e7651e3b6efb6d19e5f165
Jungkyu Yang <didwjd8590412@gmail.com>
2023-09-08 18:47:14 +0900

    chore: deploy url 설정 및 cache 기능 비활성화

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import { getCacheByKey, setCacheByExpireTime } from '../utils/cache';
-import { CACHE_DELETE_TIME, URL } from '../constants/constants';
+import { CACHE_DELETE_TIME, DEPLOY_URL } from '../constants/constants';
 
 const instance = axios.create({
-	baseURL: URL,
+	baseURL: DEPLOY_URL,
 });
 
 export const getClinicalTrial = async (query: string) => {
-	const cacheItem = await getCacheByKey(query);
+	// const cacheItem = await getCacheByKey(query);
 
-	if (cacheItem) {
-		return cacheItem;
-	}
+	// if (cacheItem) {
+	// 	return cacheItem;
+	// }
 
 	console.info('calling api');
 	const response = await instance.get(`?q=${query}`);
 
-	setCacheByExpireTime({ key: query, value: response.data, expireTime: CACHE_DELETE_TIME });
+	//setCacheByExpireTime({ key: query, value: response.data, expireTime: CACHE_DELETE_TIME });
 	return response.data;
 };
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,6 +5,8 @@ export const ROUTES = {
 
 export const URL = 'http://localhost:4000/sick';
 
+export const DEPLOY_URL = 'http://ec2-13-209-228-20.ap-northeast-2.compute.amazonaws.com:4000/sick';
+
 export const MAX_LIST_NUM = 7;
 
 export const CACHE_NAME = 'cache';
